61eff19503603932254fa7ca189a3a30588d10d9
Brandon Lai <brandon.lai@vanderbilt.edu>
2020-10-10 18:06:06 -0400

    fixes data format when writing to json file

--- recipe-api.py
+++ recipe-api.py
@@ -7,28 +7,34 @@ category = 'recipes'
 json_file = 'recipes.json'
 
 with open(csv_file, 'r') as fd:
+    # Clears outfile for rewriting
+    json_object = ""
+    with open(json_file, "w") as outfile:
+        outfile.write(json_object)
+    
     reader = csv.reader(fd)
 
-    data = {}
-    data[category] = []
+    data_total = []
 
     for row in reader:
         # Gives the url as a string from csv file
         scraper = scrape_me(row[0])
-
-        data['recipes'].append({
+        
+        data = {
             'title' : scraper.title(),
             # 'total_time' : scraper.total_time(),
             'yields' : scraper.yields(),
             'ingredients' : scraper.ingredients(),
             'instructions' : scraper.instructions(),
             'image' : scraper.image(),
-        })
+        }
 
-    # Serializing json
-    json_object = json.dumps(data, indent = 5)
+        data_total.append(data)
 
-    # Writing to recipes.json
-    with open(json_file, "w") as outfile:
-        outfile.write(json_object)
+         # Serializing json
+        json_object = json.dumps(data_total, indent = 5)
+
+         # Writing to recipes.json
+        with open(json_file, "w") as outfile:
+            outfile.write(json_object)
 
